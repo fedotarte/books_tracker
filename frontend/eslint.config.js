ed1764c8cf626848cd5b1cbc336b0ee86c0b08a2
fedotarte <artyom.fedotov93@gmail.com>
2024-09-22 14:38:10 +0300

    chore(main): [fe] fixed eslint and prettier configs

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,8 +3,10 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import mantine from "eslint-config-mantine";
 
 export default tseslint.config(
+    ...mantine,
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
